6b1a7181d7d9ffc909546094074e6c0bffb720a7
Julio <julio.alves2@aluno.cps.sp.gov.br>
2026-04-18 20:17:12 -0300

    Exercícios de variáveis em Java

---
+++ Ex1.java
@@ -0,0 +1,12 @@
+public class Ex1 {
+
+    public static void main(String[] args) {
+
+        String nome = "João";
+        int idade = 20;
+
+        System.out.println("Meu nome é " + nome + " e tenho " + idade + " anos.");
+
+    } // fecha o main
+
+} // fecha a classe
